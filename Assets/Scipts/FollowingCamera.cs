7fc81ae153e41689bb7d2cf97560f191e889ef87
jolliebrown <michealbradcute@gmail.com>
2022-08-16 17:59:39 +0700

    update Camera follow

--- Assets/Scipts/FollowingCamera.cs
+++ Assets/Scipts/FollowingCamera.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class FollowingCamera : MonoBehaviour
 {
-    [SerializeField] GameObject thingToFollow;
+    public List<Transform> targets;
+    // [SerializeField] GameObject thingToFollow;
+    public Vector3 offset;
 
     void LateUpdate()
     {
-        transform.position = thingToFollow.transform.position + new Vector3 (0, 0, -50);
+        if (targets.Count == 0)
+            return;
+        Vector3 centerPoint = GetCenterPoint();
+        transform.position = centerPoint + offset;
+    }
+
+    Vector3 GetCenterPoint()
+    {
+        if (targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+        return bounds.center;
     }
 }
